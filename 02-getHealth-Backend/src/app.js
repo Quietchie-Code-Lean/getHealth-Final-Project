8bb7cea95a31fa66f6f1e23929047a1c2cc78124
Lisandro Alvarado <lisandroalvarado1804@gmail.com>
2026-08-18 17:37:26 -0400

    feat: implement specialties backend

--- 02-getHealth-Backend/src/app.js
+++ 02-getHealth-Backend/src/app.js
@@ -1,10 +1,14 @@
 import express from "express";
 import cors from "cors";
 
+import router from "./routes/specialty.routes.js";
+
 const app = express();
 
 app.use(cors());
 
 app.use(express.json());
+
+app.use("/api/specialties", router);
 
 export default app;
