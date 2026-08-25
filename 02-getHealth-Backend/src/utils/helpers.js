f484538320bf2e5808c2350c7d38e7749f7a2f6c
Leandro Funes <leofunesar@gmail.com>
2026-08-25 21:32:13 +0100

    Revert "Merge pull request #11 from Quietchie-Code-Lean/refactor/frontend-profile"
    This reverts commit 566b62d99ed55592219ba2ead21c9e7d2732217b, reversing
    changes made to 71982cf9a60102de78c8b44c4fa61b584a309e82.

--- 02-getHealth-Backend/src/utils/helpers.js
+++
@@ -1,9 +0,0 @@
-export const emptyToUndefined = (value) => {
-    if (typeof value !== "string") {
-        return value ?? undefined;
-    }
-
-    const trimmed = value.trim();
-
-    return trimmed === "" ? undefined : trimmed;
-};
